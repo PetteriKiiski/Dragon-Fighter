f8e43acbe80436396365e86fc389d77fe103f779
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2021-08-01 23:16:31 -0700

    Glitches in code, soon done

--- DragonFighter.py
+++ DragonFighter.py
@@ -26,14 +26,47 @@ def GamePage():
 	clock = pygame.time.Clock()
 	DragonTimer = time.time()
 	NinjaO = 1
-	NinjaDist = 200
+	NinjaDist = 500
 	DragonO = 3
 	while True:
-		canvas.fill(255, 255, 255)
+		canvas.fill((255, 255, 255))
+		DragonRect = pygame.Rect(0, 0, *([100, 200] if DragonO % 2 == 1 else [200, 100]))
+		DragonRect.centerx = 615
+		DragonRect.centery = 330
+		canvas.blit(pygame.image.load("Dragon{}.png".format(DragonO)), DragonRect)
+		NinjaRect = pygame.Rect(DragonRect.centerx + (-1 if NinjaO == 2 else 1) * (NinjaDist if NinjaO % 2 == 0 else 0), DragonRect.centery + (-1 if NinjaO == 3 else 1) * (NinjaDist if NinjaO % 2 == 1 else 0), *([100, 200] if NinjaO % 2 == 1 else [200, 100]))
+		canvas.blit(pygame.image.load("Ninja{}.png".format(NinjaO)), NinjaRect)
 		if time.time() - DragonTimer >= 1.0/DragonSpeed:
-			if NinjaO == DragonO and NinjaDist == "???":
-				
+			if NinjaO == DragonO and NinjaDist <= 100:
+				print ("You Lose")
+				for event in pygame.event.get():
+					if event.type == QUIT:
+						pygame.quit()
+						sys.exit()
+			elif abs(NinjaO - DragonO) == 2:
+				DragonO += 1 if DragonO != 4 else 0
+			else:
+				if NinjaO == (DragonO + 1):
+					DragonO += 1 if DragonO != 4 else 0
+				else:
+					DragonO -= 1 if DragonO != 0 else 4
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == K_DOWN:
+				if event.key == K_LEFT:
+					NinjaO -= 1 if NinjaO != 0 else 4
+				if event.key == K_RIGHT:
+					NinjaO += 1 if NinjaO != 4 else 0
+				if event.key == K_RETURN:
+					NinjaDist -= EntrySpeed
+				if event.key == K_SPACE and NinjaDist <= 100:
+					print ("You Lose")
+					for event in pygame.event.get():
+						if event.type == QUIT:
+							pygame.quit()
+							sys.exit()
+		pygame.display.update()
 		clock.tick(10)
 HomePage()
-#1360/2
-#615
