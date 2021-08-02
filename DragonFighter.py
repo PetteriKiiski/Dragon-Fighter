6078e5c903efb6077b685999d9e33d0a855698fa
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2021-08-02 09:15:30 -0700

    Bug with dragon AI, not sure how fix

--- DragonFighter.py
+++ DragonFighter.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 pygame.init()
 EntrySpeed = 5
 #Move/second
-DragonSpeed = 1.0
+DragonSpeed = 3.0
 canvas = pygame.display.set_mode((1360, 660))
 pygame.display.set_caption("Dragon Fighter")
 Home = pygame.image.load("HomePage.png")
@@ -26,43 +26,49 @@ def GamePage():
 	clock = pygame.time.Clock()
 	DragonTimer = time.time()
 	NinjaO = 1
-	NinjaDist = 500
-	DragonO = 3
+	NinjaDist = 200
+	DragonO = 1
 	while True:
 		canvas.fill((255, 255, 255))
 		DragonRect = pygame.Rect(0, 0, *([100, 200] if DragonO % 2 == 1 else [200, 100]))
 		DragonRect.centerx = 615
 		DragonRect.centery = 330
 		canvas.blit(pygame.image.load("Dragon{}.png".format(DragonO)), DragonRect)
-		NinjaRect = pygame.Rect(DragonRect.centerx + (-1 if NinjaO == 2 else 1) * (NinjaDist if NinjaO % 2 == 0 else 0), DragonRect.centery + (-1 if NinjaO == 3 else 1) * (NinjaDist if NinjaO % 2 == 1 else 0), *([100, 200] if NinjaO % 2 == 1 else [200, 100]))
-		canvas.blit(pygame.image.load("Ninja{}.png".format(NinjaO)), NinjaRect)
+		NinjaRect = pygame.Rect(0, 0, *([100, 200] if NinjaO % 2 == 0 else [200, 100]))
+		NinjaRect.centerx = DragonRect.centerx + (-1 if NinjaO == 4 else 1) * (NinjaDist if NinjaO % 2 == 0 else 0)
+		NinjaRect.centery = DragonRect.centery + (-1 if NinjaO == 3 else 1) * (NinjaDist if NinjaO % 2 == 1 else 0)
+		img = pygame.image.load("Ninja{}.png".format(NinjaO))
+		canvas.blit(img, NinjaRect)
 		if time.time() - DragonTimer >= 1.0/DragonSpeed:
-			if NinjaO == DragonO and NinjaDist <= 100:
-				print ("You Lose")
-				for event in pygame.event.get():
-					if event.type == QUIT:
-						pygame.quit()
-						sys.exit()
+			DragonTimer = time.time()
+			if NinjaO == DragonO:
+				DragonO += 1 if DragonO != 4 else -3
 			elif abs(NinjaO - DragonO) == 2:
-				DragonO += 1 if DragonO != 4 else 0
+				if NinjaDist <= 100:
+					print ("You Lose")
+					for event in pygame.event.get():
+						if event.type == QUIT:
+							pygame.quit()
+							sys.exit()
 			else:
-				if NinjaO == (DragonO + 1):
-					DragonO += 1 if DragonO != 4 else 0
+				if NinjaO == (DragonO + (1 if DragonO != 4 else -3)):
+					print ("Annoying +")
+					DragonO += 1 if DragonO != 4 else -3
 				else:
-					DragonO -= 1 if DragonO != 0 else 4
+					DragonO -= 1 if DragonO != 1 else -3
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
-			if event.type == K_DOWN:
+			if event.type == KEYDOWN:
 				if event.key == K_LEFT:
-					NinjaO -= 1 if NinjaO != 0 else 4
+					NinjaO -= 1 if NinjaO != 1 else -3
 				if event.key == K_RIGHT:
-					NinjaO += 1 if NinjaO != 4 else 0
+					NinjaO += 1 if NinjaO != 4 else -3
 				if event.key == K_RETURN:
 					NinjaDist -= EntrySpeed
 				if event.key == K_SPACE and NinjaDist <= 100:
-					print ("You Lose")
+					print ("You Win")
 					for event in pygame.event.get():
 						if event.type == QUIT:
 							pygame.quit()
